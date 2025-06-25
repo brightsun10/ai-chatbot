691338a5d85732789fa4ade73701ccb9a6f2e3cd
Nithin P <41161616+brightsun10@users.noreply.github.com>
2025-06-25 20:31:38 +0530

    Update wsgi.py

--- dashboard/wsgi.py
+++ dashboard/wsgi.py
@@ -1,4 +1,7 @@
+# dashboard/wsgi.py
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings")
+
 application = get_wsgi_application()
