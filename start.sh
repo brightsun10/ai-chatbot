c6ddf75f4c1ca2c64b7588b7498d376b5ef0f13b
Nithin P <41161616+brightsun10@users.noreply.github.com>
2025-06-26 16:33:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate --noinput
+python manage.py makemigrations chatbot
 python manage.py collectstatic --noinput
 exec gunicorn dashboard.wsgi:application --bind 0.0.0.0:$PORT
